9118e1707b9356f08c1d93a430b057cebddad21d
Jazz Vangeel <jazzvangeel@gmail.com>
2023-11-28 13:02:34 -0500

    completed day 2 round 1

--- aoc2022/src/day02/index.ts
+++ aoc2022/src/day02/index.ts
@@ -2,42 +2,92 @@ import run from "aocrunner"
 
 const parseInput = (rawInput: string) => rawInput
 
-const gameLegend = {
-  a: 'rock',
-  b: 'paper',
-  c: 'scissors',
-  x: 'rock',
-  y: 'paper',
-  z: 'scissors'
-}
+// const gameLegend = {
+//   a: 'rock',
+//   b: 'paper',
+//   c: 'scissors',
+//   x: 'rock',
+//   y: 'paper',
+//   z: 'scissors'
+// }
 
-// if you pick
-const pointsLegend = {
-  rock: 1,
-  paper: 2,
-  scissors: 3,
-  loss: 0,
-  draw: 3,
-  win: 6
-}
+// // if you pick
+// const pointsLegend = {
+//   rock: 1,
+//   paper: 2,
+//   scissors: 3,
+//   loss: 0,
+//   draw: 3,
+//   win: 6
+// }
 
-// Frist Draft Plan? 
-// add to parse function - breaking up each line
+// can we try out object with a methods on it for fun?
+// assinging a value and doing it with math? possible but mindnumbing
 
-// Do i want to work with an array or object? 
-
-
-// I feel like a swtich/break statement could be good idea.
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
   const listOfMatches: string[] = input.split("\n")
   const arrayOfMatches: string[][] = listOfMatches.map((listOfMatches) => listOfMatches.split(" "));
 
-  console.log(arrayOfMatches, 'results?')
-  let totalPoints;
-  
+  let myTotalTournamentScore: number = 0 
 
-  return
+  arrayOfMatches.forEach((match: string[]) => {
+    switch(match[1]) {
+        case 'X': // Rock
+          myTotalTournamentScore = myTotalTournamentScore + 1
+
+          switch(match[0]) {
+            case 'A': // Rock = Draw
+              myTotalTournamentScore = myTotalTournamentScore + 3
+              break;
+            case 'B': // Paper = Loss
+              break;
+            case 'C': // Scissors = Win
+              myTotalTournamentScore = myTotalTournamentScore + 6
+              break;
+            default:
+              return
+          }
+          break;
+        case 'Y': // Paper
+          myTotalTournamentScore = myTotalTournamentScore + 2
+
+          switch(match[0]) {
+            case 'A': // Rock = Win
+              myTotalTournamentScore = myTotalTournamentScore + 6
+              break;
+            case 'B': // Paper = Draw
+              myTotalTournamentScore = myTotalTournamentScore + 3
+              break;
+            case 'C': // Scissors = Loss
+              break;
+            default:
+              return
+          }
+          break;
+        case 'Z': // Scissors
+          myTotalTournamentScore = myTotalTournamentScore + 3
+
+          switch(match[0]) {
+            case 'A': // Rock = Loss
+              break;
+            case 'B': // Paper = Win
+              myTotalTournamentScore = myTotalTournamentScore + 6
+              break;
+            case 'C': // Scissors = Draw
+              myTotalTournamentScore = myTotalTournamentScore + 3
+              break;
+            default:
+              return
+          }
+            break;
+        default:
+          return
+    }
+
+    return myTotalTournamentScore
+  })
+  return myTotalTournamentScore
 }
 
 const part2 = (rawInput: string) => {
