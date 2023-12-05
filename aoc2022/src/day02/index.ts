e4ad05aab5070550ab78bc763d11de79ddb3c3eb
Jazz Vangeel <jazzvangeel@gmail.com>
2023-12-04 19:01:33 -0500

    complete day 2 2022

--- aoc2022/src/day02/index.ts
+++ aoc2022/src/day02/index.ts
@@ -87,76 +87,206 @@ const part1 = (rawInput: string) => {
 
 const part2 = (rawInput: string) => {
   const arrayOfMatches = parseInput(rawInput)
-  let myTotalTournamentScore: number = 0 
-  const pointsLegend = {
-  rock: 1,
-  paper: 2,
-  scissors: 3,
-  loss: 0,
-  draw: 3,
-  win: 6
-}
+  let myTotalTournamentScore: number = 0;
+  // let competitorsSelection: string | undefined;
 
-  arrayOfMatches.forEach((match: string[]) => {
-    switch(match[0]) {
-        case 'A': // Rock
-          switch(match[1]) {
-            case 'X': // Lose = Paper
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper
-              break;
-            case 'Y': // Draw = Rock
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock + pointsLegend.draw
-              break;
-            case 'Z': // Win = Scissors
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors + pointsLegend.win
-              break;
-            default:
-              return
-          }
-          return;
-        case 'B': // Paper
-          switch(match[1]) {
-            case 'X': // Lose = Scissors
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors
-              break;
-            case 'Y': // Draw = Paper
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper + pointsLegend.draw
-              break;
-            case 'Z': // Win = Rock
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock + pointsLegend.win
-              break;
-            default:
-              return
-          }
-          return;
-        case 'C': // Scissors
-          switch(match[1]) {
-            case 'X': // Lose = Rock
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock
-              break;
-            case 'Y': // Draw = Scissors
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors + pointsLegend.draw
-              break;
-            case 'Z': // Win = Paper
-              myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper + pointsLegend.win
-              break;
-            default:
-              return
-          }
-            return;
-        default:
-        return
+  const pointsLegend = {
+    rock: 1,
+    paper: 2,
+    scissors: 3,
+    loss: 0,
+    draw: 3,
+    win: 6
+  }
+
+  // const gameLegend = {
+  //   a: 'rock',
+  //   b: 'paper',
+  //   c: 'scissors',
+  //   x: 'rock',
+  //   y: 'paper',
+  //   z: 'scissors'
+  // }
+
+// attempt 2
+// const map1 = arrayOfMatches.map((match) => {
+//   // match ['Y', 'R']
+//   match.reduce((myTotalTournamentScore, current:string) => {
+//     switch(current) {
+//       case 'A': // Rock
+//         competitorsSelection = 'A'
+//         break;
+//       case 'B': // Paper
+//         competitorsSelection = 'B'
+//         break;
+//       case 'C': // Scissors
+//         competitorsSelection = 'C'
+//         break;
+//       case 'X': // Lose
+//         if(competitorsSelection === 'A'){
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper
+//         } else if (competitorsSelection === 'B') {
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors
+//         } else {
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock
+//         }
+//         break;
+//       case 'Y': // Draw
+//         myTotalTournamentScore = myTotalTournamentScore + pointsLegend.draw
+//         if(competitorsSelection === 'A'){
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock
+//         } else if (competitorsSelection === 'B') {
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper
+//         } else {
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors
+//         }
+//         break;
+//       case 'Z': // Win
+//         myTotalTournamentScore = myTotalTournamentScore + pointsLegend.win
+//         if(competitorsSelection === 'A'){
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper
+//         } else if (competitorsSelection === 'B') {
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors
+//         } else {
+//           myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock
+//         }
+//         break;
+//       default:
+//         return myTotalTournamentScore;
+//         // maybe return above? 
+//   }
+//   return myTotalTournamentScore
+//   }, myTotalTournamentScore)
+
+//   return myTotalTournamentScore
+// });
+
+// attenpt 1:
+  // arrayOfMatches.forEach((match: string[]) => {
+  //   // console.log(match[0], match[1], 'do we see each match')
+  //   switch(match[0]) {
+  //       case 'A': // Rock
+         
+  //         switch(match[1]) {
+  //           case 'X': // Lose = Paper
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper
+  //             break;
+  //           case 'Y': // Draw = Rock
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock + pointsLegend.draw
+  //             break;
+  //           case 'Z': // Win = Scissors
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors + pointsLegend.win
+  //             break;
+  //           default:
+  //           return;
+  //         }
+  //       case 'B': // Paper
+  //         switch(match[1]) {
+  //           case 'X': // Lose = Scissors
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors
+  //             break;
+  //           case 'Y': // Draw = Paper
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper + pointsLegend.draw
+  //             break;
+  //           case 'Z': // Win = Rock
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock + pointsLegend.win
+  //             break;
+  //           default:
+  //           return;
+  //         }
+  //       case 'C': // Scissors
+  //         switch(match[1]) {
+  //           case 'X': // Lose = Rock
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock
+  //             break;
+  //           case 'Y': // Draw = Scissors
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors + pointsLegend.draw
+  //             break;
+  //           case 'Z': // Win = Paper
+  //             myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper + pointsLegend.win
+  //             break;
+  //           default:
+  //             return;
+  //         }
+  //       default:
+  //       break;
+  //   }
+  // })
+
+  // They have selected  - so you get these points
+  // Some sort of index
+  // const win = {
+  //   'A': 2,
+  //   'B': 3,
+  //   'C': 1,
+  // }
+  // const loss = {
+  //   A: 3, // rock
+  //   B: 1, // paper
+  //   C: 2, // scissors
+  // }
+  // const draw = {
+  //   'A': 1, // rock
+  //   'B': 2, // paper
+  //   'C': 3, // scissors
+  // }
+  // .getItem(key)
+
+  // 100p better solution but lets move on lol
+  // attempt 3
+   arrayOfMatches.forEach((match: string[]) => {
+    if(match.includes('X')){ // loss
+      if(match.includes('A')){ 
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors;
+      }
+      if(match.includes('B')){
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock;
+      }
+      if(match.includes('C')){
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper;
+      }
     }
-    console.log('For Each Match', myTotalTournamentScore)
-    return myTotalTournamentScore
+    if(match.includes('Y')){
+      myTotalTournamentScore = myTotalTournamentScore + pointsLegend.draw;
+      if(match.includes('A')){ // rock
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock;
+      }
+      if(match.includes('B')){
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper;
+      }
+      if(match.includes('C')){
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors;
+      }
+    }
+    if(match.includes('Z')){
+      myTotalTournamentScore = myTotalTournamentScore + pointsLegend.win;
+      if(match.includes('A')){ // rock
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.paper;
+      }
+      if(match.includes('B')){
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.scissors;
+      }
+      if(match.includes('C')){
+        myTotalTournamentScore = myTotalTournamentScore + pointsLegend.rock;
+      }
+    }
+    console.log(match, myTotalTournamentScore);
   })
   return myTotalTournamentScore
 }
 
-// 25630 - too high
-// 11003 - too low
+// 25630 - too hig
+// ?? 4649
+// ?? 11258
+// 11003 - too low 
 
-// left off - need to write some tests to figure out the problem. 
+// [[Y,C] ]
+
+// someone suggested 
+// isDraw
+// isWin
+// isLose
+// and assigning all the letters to strings of the words
 
 run({
   part1: {
@@ -170,10 +300,14 @@ run({
   },
   part2: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: 
+        `A Y
+        C Y
+        C X
+        A Y`,
+        expected: 15,
+      },
     ],
     solution: part2,
   },
