f91403e90d6a344e7f5159634685ff206fe26bb3
Jazz Vangeel <jazzvangeel@gmail.com>
2023-12-07 12:24:02 -0500

    part two still in progress

--- aoc2022/src/day03/index.ts
+++ aoc2022/src/day03/index.ts
@@ -1,6 +1,8 @@
 import run from "aocrunner"
 
-// find the common number item for each bag and add them all together. 
+type legendType = {
+  [key:string]:number
+}
 
 const parseInput = (rawInput: string): string[][] => {
   const listOfBackpacks = rawInput.split("\n")
@@ -13,10 +15,6 @@ const parseInput = (rawInput: string): string[][] => {
     return backpackCompartments
   });
   return listOfBrokenupBackpacks;
-}
-
-type legendType = {
-  [key:string]:number
 }
 
 function createLegend() {
@@ -43,10 +41,20 @@ function createLegend() {
   return legend
 }
 
-function findSame(str1: string, str2: string) {
+function findSame(str1: string, str2: string, str3?: string) {
   const set1 = new Set(str1);
   const commonCharacters = Array.from(set1).filter(char => str2.includes(char)).join('');
   return commonCharacters;
+}
+
+// Assign by Troop
+function groupElvesByTroop(allBackpacks: string[]) {
+  const troopSize = 3
+  let troops
+  for (let i = 0; i < allBackpacks.length; i += troopSize) {
+    troops = allBackpacks.slice(i, i + troopSize)
+  }
+  return troops
 }
 
 const part1 = (rawInput: string) => {
@@ -66,8 +74,23 @@ const part1 = (rawInput: string) => {
   return backpackValue
 }
 
+// Elves are divided into groups of 3 - check / not a check, its only running for so many
+// They all should have one matching item in common, in at least one of the compartments in their rucksucks
+// They are tranlated as much
+// Add them all up.
+// Dont need the compartment - check
+
 const part2 = (rawInput: string) => {
-  const input = parseInput(rawInput)
+  const listOfBackpacks = rawInput.split("\n");
+  const elfsByTroop = groupElvesByTroop(listOfBackpacks);
+
+  // Find the matching item in each troop
+  const sameLetters = elfsByTroop?.map((troop) => {
+    return findSame(troop[0], troop[2], troop[3])
+  })
+
+  console.log(sameLetters, 'what same letters do we get from troop 1 and two')
+
 
   return
 }
