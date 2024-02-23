53b9c4387526e22f0b2ee260d73241c02205ead0
Jazz Vangeel <jazzvangeel@gmail.com>
2024-02-23 14:05:32 -0500

    completed day 1 part 1

--- aoc2023/src/day01/index.ts
+++ aoc2023/src/day01/index.ts
@@ -1,11 +1,40 @@
 import run from "aocrunner"
 
-const parseInput = (rawInput: string) => rawInput
+const parseInput = (rawInput: string) => {
+
+  // order does not matter here, could we return an object?
+  const arrayOfStrings = rawInput.split('\n')
+  return arrayOfStrings 
+}
+
+// const reverseString = (str : string) => {
+//   return str.split("").reverse().join("");
+// }
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput)
+  let codes: number[] = []
 
-  return
+
+
+  // swtiching for something that returns the array 
+  for (let line = 0; line < input.length; line++){
+    const instruction: string[] = input[line].split("");
+
+    // serach for numbers.
+    const numbers = instruction.filter(character => /\d/.test(character));
+    const firstNumber = numbers[0]
+    const lastNumber = numbers[numbers.length - 1]
+    const total = firstNumber + lastNumber
+
+    codes.push(parseInt(total))
+  }
+
+  const totalCalibrationValues = codes.reduce((acc, code) => {
+    return acc + code
+  }, 0)
+  
+  return totalCalibrationValues
 }
 
 const part2 = (rawInput: string) => {
